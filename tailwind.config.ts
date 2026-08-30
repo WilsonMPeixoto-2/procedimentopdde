62c1f7393843b3e921c07ee79e54a56d9897cd3e
Wilson Malafaia Peixoto <wilsonmp2@gmail.com>
2026-08-30 19:45:05 -0300

    fix(tailwind): write ESM imports with real newlines

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
-import type { Config } from "tailwindcss";\nimport typography from "@tailwindcss/typography";\nimport tailwindcssAnimate from "tailwindcss-animate";
+import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
   darkMode: ["class"],
